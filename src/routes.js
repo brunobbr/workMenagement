6adba93df7a6926fd6a88ec336fab88f78bd65b9
brunobbr <bruno-loll@hotmail.com>
2021-04-07 17:48:19 -0400

    bug

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const routes = express.Router()
 
-const basePath = __dirname + "/views"
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "Bruno",
+    avatar: "https://avatars.githubusercontent.com/u/63427164?s=400&u=b0504a2df7d06c9784a2b6897506b415f65efd78&v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 6,
+    "vacation-per-year": 2
+}
 
 // request, response
-routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"))
-routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"))
-routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"))
-routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"))
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", {profile}))
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
+server.set('view engine', 'ejs')
 
 //habilitar arquivos static
 server.use(express.static("public"))
