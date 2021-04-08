a72f3204fbc00595df21eea20a5ddef79f188e80
brunobbr <bruno-loll@hotmail.com>
2021-04-08 17:48:27 -0400

    bug progress

--- src/routes.js
+++ src/routes.js
@@ -9,19 +9,66 @@ const profile = {
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 6,
-    "vacation-per-year": 2
+    "vacation-per-year": 2,
+    "value-hour": 25
 }
 
 const jobs = [
-    {},
-    {},
+    {
+        id: 1,
+        name: "Feito com Amor",
+        "daily-hours": 2,
+        "total-hours": 1,
+        created_At: Date.now(),
+    },
+    {
+        id: 2,
+        name: "Designer",
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_At: Date.now(),
+    }
 ]
 
+function remainingDays(job){
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+    const createdDate = new Date(job.created_At)
+    const dueDay = createdDate.getDate() + Number(remainingDays)
+    const dueDateInMs = createdDate.setDate(dueDay)
+    const timeDiffInMs = dueDateInMs - Date.now()
+    const dayInMs = 1000 * 60 * 60 * 24
+    const dayDiff = (timeDiffInMs / dayInMs).toFixed()
+
+    return dayDiff
+}
+
 // request, response
-routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/', (req, res) => {
+    const updatedJobs = jobs.map((job) => {
+       const remaining = remainingDays(job)
+       const status = remaining <= 0 ? 'done' : 'progress'
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile["value-hour"] * job["total-hours"]
+        }
+    })
+    
+
+    return res.render(views + "index", {jobs: updatedJobs})
+})
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.post('/job', (req, res) => {
-    jobs.push(req.body)
+
+    const lastId =  jobs[jobs.length -1]?.id || 1;
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_At: Date.now()// atribuindo nova data
+    })
     return res.redirect('/')
 })
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
